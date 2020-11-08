6ba2e30040ba122baab8a96887503bd0f693d5e8
Milind Mohapatra <milind.mohapatra@ltts.com>
2020-11-08 17:10:10 +0530

    Fix assert statements

--- JSMiniProject/src/test/java/TestRunner/TestRunner.java
+++ JSMiniProject/src/test/java/TestRunner/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = "src/test/resources/Feature", glue = { "StepDefinition" }, plugin = {
+@CucumberOptions(features = "src/test/resources/Feature1", glue = { "StepDefinition" }, plugin = {
 		"html:target/Report-html" }, monochrome = true)
 
 public class TestRunner {
