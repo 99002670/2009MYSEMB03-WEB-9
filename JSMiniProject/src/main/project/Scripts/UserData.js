d68b28adb6d5e37b7470aa96adae6b52b0f92be2
Amit Das <amit.das@ltts.com>
2020-11-10 01:13:08 +0530

    Registration Online

--- JSMiniProject/src/main/project/Scripts/UserData.js
+++ JSMiniProject/src/main/project/Scripts/UserData.js
@@ -1,4 +1,5 @@
 class UserData {
+    
     constructor(name, email, phone, password) {
         this.name = name;
         this.email = email;
@@ -6,20 +7,33 @@ class UserData {
         this.password = password;
     }
 
-    display() {
-        console.log(
-            "Name: " +
-                this.name +
-                "\n" +
-                "Email: " +
-                this.email +
-                "\n" +
-                "Phone: " +
-                this.phone +
-                "\n" +
-                "Password: " +
-                this.password +
-                "\n"
-        );
+    // display() {
+    //     console.log(
+    //         "Name: " +
+    //             this.name +
+    //             "\n" +
+    //             "Email: " +
+    //             this.email +
+    //             "\n" +
+    //             "Phone: " +
+    //             this.phone +
+    //             "\n" +
+    //             "Password: " +
+    //             this.password +
+    //             "\n"
+    //     );
+    // }
+
+    register(){
+        const url = "http://localhost:8000/userData"
+        const axios = require('axios');
+
+        axios.post('url', {
+            id : 0
+        }).then(resp => {
+            console.log(resp.data);
+        }).catch(error => {
+            console.log(error);
+        });   
     }
 }
